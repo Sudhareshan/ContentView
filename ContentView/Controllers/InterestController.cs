99e769aab1a7b07375f72578a39bfcac9475e680
Your Sudharshan <sudhareshanmech@gmail.com>
2023-08-31 15:28:11 +0530

    Add project files.

---
+++ ContentView/Controllers/InterestController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ContentView.Models;
+using System.Text;
+using System.Reflection;
+
+namespace ContentView.Controllers
+{
+    public class InterestController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(InterstModel im)
+        {
+            decimal simpleInterst = (im.Amount * im.Year * im.Rate) / 100;
+
+            StringBuilder sbInterst= new StringBuilder();
+            sbInterst.Append("Amount :" + im.Amount + "\n");
+            sbInterst.Append("Rate :" + im.Rate + "\n");
+            sbInterst.Append("Time(year) :" + im.Year + "\n");
+            sbInterst.Append("Interest :" + simpleInterst);
+
+            return Content(sbInterst.ToString());
+        }
+    }
+}
---
+++ ContentView/Models/InterstModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ContentView.Models
+{
+    public class InterstModel
+    {
+        public decimal Amount { get; set; }
+        public decimal Rate { get; set; }
+        public int Year { get; set; }
+    }
+}
